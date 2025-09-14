090c24e945b237f6b0666acaf8b032cfc1f52201
shivank mangal <shivankmangal6@gmail.com>
2025-09-15 01:49:46 +0530

    Dsa day 6

---
+++ src/DSA/BubbleSort.java
@@ -0,0 +1,39 @@
+package DSA;
+
+public class BubbleSort {
+    public static void main(String[] args){
+        int n = 6;
+        int[] arr = { 2,1,4,5,63,3 };
+        bubbleSort(arr,n);
+        printArray(arr, n);
+    }
+    // i=0:- j=5,4,3,2,1
+
+    private static void bubbleSort(int[] arr,int n)
+    {
+        for(int i=0; i<n; i++){
+            boolean isSwap = false;
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwap = true;
+                }
+            }
+            if(!isSwap){ // for already sorted array
+                return;
+            }
+        }
+    }
+
+    private static void printArray(int[] arr, int n)
+    {
+      for(int i=0; i<n; i++){
+          System.out.println(arr[i]);
+      }
+    }
+}
+
+// printing in ascending order
+// time complexity is O(n^2)
---
+++ src/DSA/SelectionSort.java
@@ -0,0 +1,31 @@
+package DSA;
+
+public class SelectionSort
+{
+    public static void main(String[] args){
+        int n = 5;
+        int[] arr = { 2,1,4,6,5 };
+        selectionSort(arr, n);
+        printArray(arr,n);
+    }
+
+    private static void selectionSort(int[] arr, int n) {
+        for(int i=0; i<n-1; i++){
+            int minIndex = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    static void printArray(int[] arr, int n){
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
